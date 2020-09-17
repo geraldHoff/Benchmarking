ae64dbad5dbfc996bda665419758b7c6272116d4
gerald <greengorilla3@protonmail.com>
2020-09-17 14:29:43 -0700

    The createList method now sorts the numbers read from the file into a list.

--- src/BenchMain.java
+++ src/BenchMain.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class BenchMain {
@@ -48,12 +49,27 @@ public class BenchMain {
         LinkedList<Integer> list = new LinkedList<>();
         Scanner inputScanner = new Scanner(input);
 
-        while(inputScanner.hasNext()){
+        //check if has int
+        if (inputScanner.hasNextInt()) {
             list.add(inputScanner.nextInt());
+
+            while (inputScanner.hasNextInt()) {
+
+                ListIterator<Integer> listIterator = list.listIterator();
+
+                int current = inputScanner.nextInt();
+
+                //sort ints as inserted
+                for (int i = 0; i < list.size(); i++){
+                    if (listIterator.next() > current){
+                        list.add(i, current);
+                        break;
+                    }
+                }
+
+            }
+            return list;
         }
-
-
-
-        return list;
+        return null;
     }
 }
